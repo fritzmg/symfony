52e4d009460eaac90c0fa053cf4be2ddbd6cfc2e
Fritz Michael Gschwantner <fmg@inspiredminds.at>
2025-08-10 13:11:03 +0100

    use isInitializedConfigEnabled

--- src/Symfony/Bundle/FrameworkBundle/DependencyInjection/FrameworkExtension.php
+++ src/Symfony/Bundle/FrameworkBundle/DependencyInjection/FrameworkExtension.php
@@ -416,6 +416,7 @@ class FrameworkExtension extends Extension
         }
 
         if ($this->readConfigEnabled('mailer', $container, $config['mailer'])) {
+            $this->readConfigEnabled('rate_limiter', $container, $config['rate_limiter']);
             $this->registerMailerConfiguration($config['mailer'], $container, $loader, $this->readConfigEnabled('webhook', $container, $config['webhook']));
 
             if (!$this->hasConsole() || !class_exists(MailerTestCommand::class)) {
@@ -2876,7 +2877,7 @@ class FrameworkExtension extends Extension
             }
         }
 
-        if ($transportRateLimiterReferences && $this->readConfigEnabled('rate_limiter', $container, $config['rate_limiter'])) {
+        if ($transportRateLimiterReferences && $this->isInitializedConfigEnabled('rate_limiter')) {
             if (!interface_exists(LimiterInterface::class)) {
                 throw new LogicException('Rate limiter cannot be used within Mailer as the RateLimiter component is not installed. Try running "composer require symfony/rate-limiter".');
             }
